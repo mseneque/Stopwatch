b8603024bfc3cbc17b81b6989a5e59e9a0cf1f7e
mseneque <mattseneque@gmail.com>
2017-08-23 09:53:24 +0800

    Initial Commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            Console.WriteLine("Stopwatch commands:\ns - start/stop\nq - quit");
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "s":
+                        var timeElapsed = stopwatch.StartStop();
+                        var status = stopwatch.Status == "stopped" ? "Stopped" : "Started";
+                        Console.WriteLine("{0} at {1:hh}:{1:mm}:{1:ss}", status, timeElapsed);
+                        break;
+
+                    case "q":
+                        return;
+
+                    default:
+                        Console.WriteLine("Not a valid entry");
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ StopWatch/Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace StopWatch
+{
+    enum State { stopped, timing }
+
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+
+        private State _state;
+
+        public String Status
+        {
+            get
+            {
+                return _state.ToString();
+            }
+        }
+
+        public Stopwatch()
+        {
+            _state = State.stopped;
+        }
+
+
+        private TimeSpan _Start()
+        {
+            _startTime = DateTime.Now;
+            _state = State.timing;
+            return _startTime - _startTime;
+        }
+
+        private TimeSpan _Stop()
+        {
+            _state = State.stopped;
+            return DateTime.Now - _startTime;
+        }
+
+        public TimeSpan StartStop()
+        {
+            if (_state == State.stopped)
+                return _Start();
+            return _Stop();
+        }
+
+
+
+    }
+}
